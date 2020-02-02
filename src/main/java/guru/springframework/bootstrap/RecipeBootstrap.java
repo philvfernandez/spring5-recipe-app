bae4fc523a9b0117669cbd80500f25ca6e125a3d
Phil Fernandez <philvfernandez@gmail.com>
2020-02-01 18:46:03 -0700

    Just added one comment to bootstrap class for refactoring exercise in next section.

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -122,6 +122,8 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "\n" +
                 "\n" +
                 "Read more: http://www.simplyrecipes.com/recipes/perfect_guacamole/#ixzz4jvoun5ws");
+
+        //needed for bidirectional - should be one method call;
         guacNotes.setRecipe(guacRecipe);
         guacRecipe.setNotes(guacNotes);
 
