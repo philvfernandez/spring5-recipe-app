c28bd2233b01a5777dc61bad115ffd84b6492739
Phil Fernandez <philvfernandez@gmail.com>
2020-05-25 16:24:15 -0600

    Added specific error on 404 error page.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -58,11 +58,13 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
+    public ModelAndView handleNotFound(Exception e) {
         log.error("Handling Not Found Exception");
+        log.error(e.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", e);
         return modelAndView;
     }
 }
