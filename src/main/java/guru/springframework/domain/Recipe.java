67fc726970e809a73ebca2fdb855495711fb0522
Phil Fernandez <philvfernandez@gmail.com>
2020-12-07 17:25:25 -0700

    Finished adding the schema generation using hibernate code.  Also refactored code to use different boot strap code depending on the environment.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -39,7 +39,7 @@ public class Recipe {
     private Notes notes;
 
     @ManyToMany
-    @JoinTable(name = "recipe_cateogry",
+    @JoinTable(name = "recipe_category",
         joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
